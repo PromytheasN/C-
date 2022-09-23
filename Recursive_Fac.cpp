b85b340035144652392d419dcdc06f1336c918f7
PromytheasN <63561890+PromytheasN@users.noreply.github.com>
2022-09-23 11:20:04 -0700

    Update Recursive_Fac.cpp

--- Recursive_Fac.cpp
+++ Recursive_Fac.cpp
@@ -1,20 +1,18 @@
-//Recursive Factorial
 #include <iostream>
 using namespace std;
 
-int recursive_fac(int n){
-
-    if (n >= 1)
-        return n * recursive_fac(n - 1);
-    else
-        return 1;
+int factorial(int n){
+    int result = 1;
+    //Base case
+    if(n == 1){
+        return result;
+    }
+    result = n * factorial(n-1);
+    return result;
 }
 
 int main(){
 
-    int num;
-    cout << "Enter a positive number! " << endl;
-    cin >> num;
-    cout << "The recursive num is " << recursive_fac(num);
-
+    cout << factorial(3) << endl;
+    return 0;
 }
